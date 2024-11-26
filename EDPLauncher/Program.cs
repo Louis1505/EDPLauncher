5bb65040f7856d400fd5bca41c2bab356019e0c8
Louis1505 <louistrenkner@t-online.de>
2024-11-26 19:12:39 +0100

    V1.0.3.1

--- EDPLauncher/Program.cs
+++ EDPLauncher/Program.cs
@@ -1,6 +1,8 @@
 using System.Diagnostics;
 using System.Net.Http;
 using System.Reflection;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace EDPLauncher
 {
@@ -23,6 +25,8 @@ namespace EDPLauncher
             {
                 try
                 {
+                    AutoClosingMessageBox.Show("EDPLauncher führt ein Update durch....", "Update", 5000);
+
                     // Neue Datei herunterladen
                     DownloadNewVersion(downloadUrl, downloadPath).Wait();
 
@@ -100,5 +104,36 @@ namespace EDPLauncher
                 throw;
             }
         }
+    }
+
+    public class AutoClosingMessageBox
+    {
+        private System.Threading.Timer _timeoutTimer;
+        private string _caption;
+        private AutoClosingMessageBox(string text, string caption, int timeout)
+        {
+            _caption = caption;
+            _timeoutTimer = new System.Threading.Timer(OnTimerElapsed, null, timeout, System.Threading.Timeout.Infinite);
+            MessageBox.Show(text, caption);
+        }
+
+        public static void Show(string text, string caption, int timeout)
+        {
+            new AutoClosingMessageBox(text, caption, timeout);
+        }
+
+        private void OnTimerElapsed(object state)
+        {
+            IntPtr mbWnd = FindWindow(null, _caption);
+            if (mbWnd != IntPtr.Zero)
+                SendMessage(mbWnd, WM_CLOSE, IntPtr.Zero, IntPtr.Zero);
+            _timeoutTimer.Dispose();
+        }
+
+        private const int WM_CLOSE = 0x0010;
+        [System.Runtime.InteropServices.DllImport("user32.dll", SetLastError = true)]
+        private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+        [System.Runtime.InteropServices.DllImport("user32.dll", CharSet = System.Runtime.InteropServices.CharSet.Auto)]
+        private static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
     }
 }
